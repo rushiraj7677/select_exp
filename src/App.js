d5e5ef860767550e92617575e31a8fdf8279c59a
rushi <bhamanirushi2121@gmail.com>
2022-10-14 12:05:35 +0530

    change in grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Exp_grid from "./components/exp_grid";
 function App() {
   return (
-    <div className="App">
-      
-      <div className="container"></div>
+      <div> 
+        <h1>Select Expression</h1>                                         
+      <div className="container">
+    
       <Exp_grid />
-    </div>
+      </div>
+      </div>
   );
 }
 
